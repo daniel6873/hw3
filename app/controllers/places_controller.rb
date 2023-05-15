ea561323f24d83485ef841fbc4b69adf069bea41
Daniel Mobayyen <daniel.mobayyen@kellogg.northwestern.edu>
2023-05-15 01:09:04 +0000

    Places controller changed

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,24 @@
+class CompaniesController < ApplicationController
+
+  def index
+    @places = Place.all
+  end
+
+  def show
+    @place = Place.find_by({"id" => params["id"]})
+    @posts = Post.where("place_id" => @company["id"])
+  end
+
+  def new
+    @place = Place.new
+  end 
+
+  def create
+    @place = Place.new
+    @place["name"]=params["place"]["name"]
+    @place.save
+    redirect_to "/places"
+  end 
+
+
+end 
